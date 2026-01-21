c3c403e1cc5e31c210a6601938df631a0dd0ce99
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-01-21 14:11:23 +0800

    Update next.config.ts
    https://raw.githubusercontent.com/free-whiteboard-online

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    localPatterns: [
+      {
+        pathname: '/public/**',
+        search: '',
+      },
+    ],
+      remotePatterns: [new URL('https://raw.githubusercontent.com/free-whiteboard-online/**')],
+  },
+  
 };
 
 export default nextConfig;
