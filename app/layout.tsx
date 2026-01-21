ef98ef769ecd0f44a92d2c8f27041d39a9d6d139
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-01-21 13:56:57 +0800

    Update header layout.tsx
    Update header layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { HeaderSection } from "@/components/sections/header-section";
 import { FooterSection } from "@/components/sections/footer-section";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <HeaderSection />
         {children}
         <FooterSection />
       </body>
