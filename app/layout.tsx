883ebc33e713558ddf1ef32dded0da916c4f3edc
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-01-21 13:47:04 +0800

    Update layout.tsx footer
    Update layout.tsx footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { FooterSection } from "@/components/sections/footer-section";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -28,6 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <FooterSection />
       </body>
     </html>
   );
