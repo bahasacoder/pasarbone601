0fcccb5a99ae5a264f7a8c58cbbee59bce9b202d
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-01-22 11:56:37 +0800

    Create fake shop page.tsx
    Create fake shop page.tsx

---
+++ app/fakeshop/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+// import Navbar from "@/components/Navbar"
+// import CartList from "@/components/CartList"
+import ProductList from "@/components/products-list"
+
+
+export default function RootPage() {
+  return (
+    <>
+        <ProductList />
+    </>
+  )
+}
