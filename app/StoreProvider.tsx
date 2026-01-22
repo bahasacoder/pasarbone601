99b2d3ae1792f529da798a075b30af2483348398
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-01-22 11:51:14 +0800

    Add files via upload

---
+++ app/StoreProvider.tsx
@@ -0,0 +1,18 @@
+// /app/store/ReduxProvider.js
+'use client';
+
+import { Provider } from 'react-redux';
+import { persistStore } from "redux-persist";
+import { PersistGate } from 'redux-persist/integration/react'
+import { store, persistor } from '@/lib/store';
+
+export default function StoreProvider({ children, }: {
+  children: React.ReactNode;}) {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>       
+            {children}
+      </PersistGate>   
+    </Provider>
+  )
+}
