7e74ca36849eb189cbef18b64a70eae5296547df
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-01-22 13:48:06 +0800

    Update product list page.tsx

--- app/fakeshop/page.tsx
+++ app/fakeshop/page.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 // import Navbar from "@/components/Navbar"
 // import CartList from "@/components/CartList"
-import ProductList from "@/components/products-list"
+import ProductsList from "@/components/products-list"
 
 
 export default function ShopPage() {
   return (
     <>
-        <ProductList />
+        <ProductsList />
     </>
   )
 }
