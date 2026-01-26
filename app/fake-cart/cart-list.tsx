f77d3f6fa03c991e0860123f2e45917aab61ab43
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-01-26 13:16:03 +0800

    Fix destructuring of cart items in CartShop
    product cart-list

--- app/fake-cart/cart-list.tsx
+++ app/fake-cart/cart-list.tsx
@@ -14,7 +14,7 @@ import { useAppSelector } from '@/lib/hooks';
 export default function CartShop() {
   const dispatch = useDispatch();
   
-  const { cartItems, totalQuantity, totalAmount } = useAppSelector((state) => state.cart);
+  const { product, totalQuantity, totalAmount } = useAppSelector((state) => state.cart);
   
   const handleRemove = (id: any) => {
     dispatch(removeFromCart(id));
